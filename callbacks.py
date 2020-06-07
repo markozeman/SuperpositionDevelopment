c1ca41bba03402ed360db6ec209fdc567383a58e
markozeman <mz4730@student.uni-lj.si>
2020-06-07 18:09:22 +0200

    Callbacks added

---
+++ callbacks.py
@@ -0,0 +1,91 @@
+from math import exp
+
+from keras.callbacks import Callback, LearningRateScheduler
+import numpy as np
+
+
+class TestPerformanceCallback(Callback):
+    """
+    Callback class for testing normal model performance at the beginning of every epoch.
+    """
+    def __init__(self, X_test, y_test, model):
+        super().__init__()
+        self.X_test = X_test
+        self.y_test = y_test
+        self.model = model  # this is only a reference, not a deep copy
+        self.accuracies = []
+
+    def on_epoch_begin(self, epoch, logs=None):
+        loss, accuracy = self.model.evaluate(self.X_test, self.y_test, verbose=2)
+        self.accuracies.append(accuracy * 100)
+
+
+class TestSuperpositionPerformanceCallback(Callback):
+    """
+    Callback class for testing superposition model performance at the beginning of every epoch.
+    """
+    def __init__(self, X_test, y_test, context_matrices, model, task_index):
+        super().__init__()
+        self.X_test = X_test
+        self.y_test = y_test
+        self.context_matrices = context_matrices
+        self.model = model  # this is only a reference, not a deep copy
+        self.task_index = task_index
+        self.accuracies = []
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if self.task_index == 0:    # first task (original MNIST images) - we did not use context yet
+            loss, accuracy = self.model.evaluate(self.X_test, self.y_test, verbose=2)
+            self.accuracies.append(accuracy * 100)
+            return
+
+        # save current model weights (without bias node)
+        curr_w_matrices = []
+        curr_bias_vectors = []
+        for layer in self.model.layers[1:]:  # first layer is Flatten so we skip it
+            curr_w_matrices.append(layer.get_weights()[0])
+            curr_bias_vectors.append(layer.get_weights()[1])
+
+        # temporarily change model weights to be suitable for first task (without bias node)
+        for i, layer in enumerate(self.model.layers[1:]):  # first layer is Flatten so we skip it
+            # not multiplying with inverse because inverse is the same in binary superposition with {-1, 1} on the diagonal
+            # using only element-wise multiplication on diagonal vectors for speed-up
+            context_inverse_multiplied = np.diagonal(self.context_matrices[self.task_index][i])
+            for task_i in range(self.task_index - 1, 0, -1):
+                context_inverse_multiplied = np.multiply(context_inverse_multiplied, np.diagonal(self.context_matrices[task_i][i]))
+            context_inverse_multiplied = np.diag(context_inverse_multiplied)
+
+            layer.set_weights([context_inverse_multiplied @ curr_w_matrices[i], curr_bias_vectors[i]])
+
+        # evaluate accuracy
+        loss, accuracy = self.model.evaluate(self.X_test, self.y_test, verbose=2)
+        self.accuracies.append(accuracy * 100)
+
+        # change model weights back (without bias node)
+        for i, layer in enumerate(self.model.layers[1:]):  # first layer is Flatten so we skip it
+            layer.set_weights([curr_w_matrices[i], curr_bias_vectors[i]])
+
+
+lr_over_time = []   # global variable to store changing learning rates
+
+
+def lr_scheduler(epoch, lr):
+    """
+    Learning rate scheduler function to set how learning rate changes each epoch.
+
+    :param epoch: current epoch number
+    :param lr: current learning rate
+    :return: new learning rate
+    """
+    global lr_over_time
+    lr_over_time.append(lr)
+    decay_type = 'exponential'
+    if decay_type == 'linear':
+        lr -= 10 ** -5
+    elif decay_type == 'exponential':
+        initial_lr = 0.0001
+        k = 0.07
+        t = len(lr_over_time) % 10      # to start each new task with the same learning rate as the first one (1 task - 10 epochs)
+        lr = initial_lr * exp(-k * t)
+    return max(lr, 0.000001)    # don't let learning rate go to 0
+
